fcaadc608ecc53a2c0bcd528a8910e73fe99180c
shreethaar <shreethaara380@gmail.com>
2025-08-11 21:18:04 +0800

    improve code for better example

--- simple-dll/dll.c
+++ simple-dll/dll.c
@@ -1,24 +1,35 @@
 #include <windows.h>
 
-
 /* Export a very small function */
 __declspec(dllexport) int add(int a, int b) {
-    return a + b;
+    return a+b;
 }
-// DllMain as entrypoint 
+
+__declspec(dllexport) char* getString() {
+    static char buffer[] = "Hello from DLL";
+    return buffer;
+}
+
+__declspec(dllexport) void processData(char* input, int length) {
+    for(int i=0;i<length;i++) {
+        input[i]^=0x42;
+    }
+}
+
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
     switch (fdwReason) {        // perform actions based on the reason of calling 
-    case DLL_PROCESS_ATTACH:    // initialize once for each new process
-        break;
-
-    case DLL_THREAD_ATTACH:     // do thread-specific initialization
-        break;
-
-    case DLL_THREAD_DETACH:     // do thread-specific cleanup
-        break;
-
-    case DLL_PROCESS_DETACH:    // do not do cleanup if process termination scenario
-        break;
-    }
-    return TRUE;
+        case DLL_PROCESS_ATTACH:    // initialize once for each new process
+            OutputDebugStringA("DLL Loaded\n");
+            break;
+        case DLL_THREAD_ATTACH:     // do thread-specific initialization
+            break;
+        case DLL_THREAD_DETACH:     // do thread-specific cleanup
+            break;
+        case DLL_PROCESS_DETACH:    // do not do cleanup if process termination scenario
+        OutputDebugStringA("DLL Unloaded\n");
+            break;
+        }
+        return TRUE;
 }
+
+
--- simple-loader/simple-loader.c
+++ simple-loader/simple-loader.c
@@ -1,19 +1,23 @@
-// simple-loader.c
 #include <windows.h>
 #include <stdio.h>
+#include <string.h>
 
+// Function pointer typedefs
 typedef int (*add_func_t)(int, int);
+typedef void (*processData_func_t)(char*, int);
 
 int main(void) {
+    char string[] = "Hello from EXE";
+    printf("%s\n", string);
+    
     const char* dllName = "simple-dl.dll"; /* the DLL filename */
-
     HMODULE h = LoadLibraryA(dllName);
     if (!h) {
         DWORD err = GetLastError();
         printf("LoadLibraryA failed (error %lu). Make sure %s is in the same folder.\n", err, dllName);
         return 1;
     }
-
+    
     /* Get address of exported function "add" */
     add_func_t add = (add_func_t)GetProcAddress(h, "add");
     if (!add) {
@@ -22,13 +26,21 @@ int main(void) {
         FreeLibrary(h);
         return 1;
     }
-
-    /* Call it */
+    
+    /* Call add function */
     int a = 7, b = 5;
     int result = add(a, b);
     printf("add(%d, %d) = %d\n", a, b, result);
-
-    /* Done */
+    /* Get address of processData function */
+    processData_func_t processData = (processData_func_t)GetProcAddress(h, "processData");
+    if (!processData) {
+        printf("GetProcAddress for processData failed.\n");
+    } else {
+        char testData[] = "yappare is the best hacker\n";
+        printf("Original: %s\n", testData);
+        processData(testData, 6);
+        printf("Processed: %s\n", testData);
+    }
     FreeLibrary(h);
     return 0;
 }
